5e195718e0bcc8ac9d1479d9476f52f96f3bcf70
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-04-25 22:19:38 +0100

    cleanup router stack of root layout on logout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,29 @@
-import { Stack } from "expo-router";
-
+import { Slot, Stack } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
-import { UserProvider } from "../contexts/UserContent";
+import { UserContext, UserProvider } from "../contexts/UserContent";
+import { useContext, useEffect, useState } from "react";
 
 NativeWindStyleSheet.setOutput({ default: "native" });
 
 const RootLayout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)/music" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)/users" options={{ headerShown: false }} />
-    </Stack>
-  );
+  const { user } = useContext(UserContext);
+  const [route, setRoute] = useState<any>();
+
+  useEffect(() => {
+    setRoute(
+      user.username !== "" ? (
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/music" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/users" options={{ headerShown: false }} />
+        </Stack>
+      ) : (
+        <Slot />
+      )
+    );
+  }, [user]);
+
+  return route;
 };
 
 export default () => {
