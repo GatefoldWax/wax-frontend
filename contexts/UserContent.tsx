2e8632c5bf47df83b20488a9573fa91940a27232
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-04-25 20:33:25 +0100

    fix type definitions in user context

--- contexts/UserContent.tsx
+++ contexts/UserContent.tsx
@@ -1,14 +1,25 @@
-import { createContext, useState } from "react";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
 
 interface UserContext {
-  user: { username: string , following: string[]};
-  setUser: Function;
+  user: { username: string; following: string[] };
+  setUser: Dispatch<
+    SetStateAction<{
+      username: string;
+      following: never[];
+    }>
+  >;
 }
 
 export const UserContext = createContext<UserContext>({} as UserContext);
 
-export const UserProvider = ({ children }: { children: any }) => {
-  const [user, setUser] = useState({ username: "" , following: []});
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState({ username: "", following: [] });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
