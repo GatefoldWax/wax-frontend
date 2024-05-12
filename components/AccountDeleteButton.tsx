99a2ba2bdf0a42dfe0afdc6491ae651bf21b8530
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-05-12 19:11:54 +0100

    remove unused import

--- components/AccountDeleteButton.tsx
+++ components/AccountDeleteButton.tsx
@@ -5,7 +5,6 @@ import { Alert, Modal, Pressable, ScrollView, Text, View } from "react-native";
 import * as accountDeletion from "../constants/text/accountDeletion.json";
 import { supabase } from "../lib/supabase";
 import { FormButton } from "./reusable-components/FormButton";
-import { PostgrestError } from "@supabase/supabase-js";
 import LoadingSpinner from "./reusable-components/LoadingSpinner";
 
 const AccountDeleteButton = () => {
@@ -29,51 +28,49 @@ const AccountDeleteButton = () => {
     <View className="p-4 mx-auto">
       <Text className="p-4 my-auto text-lg">{accountDeletion.notice}</Text>
 
-      <View className="bg-[#B56DE4]">
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View className="w-[90%] mx-[5%] h-[90%] bg-[#d8bbff] absolute inset-x-0 bottom-0 rounded-t-xl p-2 ">
-            <View>
-              <Pressable
-                className="justify-self-end ml-[94%]"
-                onPress={() => {
-                  setModalVisible(!modalVisible);
-                }}
-              >
-                <AntDesign name="close" size={24} color="black" />
-              </Pressable>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View className="w-[90%] mx-[5%] h-[90%] bg-[#d8bbff] absolute inset-x-0 bottom-0 rounded-t-xl p-2 ">
+          <View>
+            <Pressable
+              className="justify-self-end ml-[94%]"
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <AntDesign name="close" size={24} color="black" />
+            </Pressable>
 
-              <ScrollView
-                contentInsetAdjustmentBehavior="scrollableAxes"
-                className="h-[80%] mx-3"
-              >
-                <Text className="text-lg my-auto">
-                  {accountDeletion.confirmation}
-                </Text>
-              </ScrollView>
+            <ScrollView
+              contentInsetAdjustmentBehavior="scrollableAxes"
+              className="h-[80%] mx-3"
+            >
+              <Text className="text-lg my-auto">
+                {accountDeletion.confirmation}
+              </Text>
+            </ScrollView>
 
-              <Pressable
-                onPress={handleDeleteAccount}
-                disabled={loading}
-                className="bg-[#ffffffc0] w-40 p-4 mt-3 rounded-md mx-auto"
-              >
-                <Text className="text-center">Accept and continue</Text>
-              </Pressable>
-            </View>
+            <Pressable
+              onPress={handleDeleteAccount}
+              disabled={loading}
+              className="bg-[#ffffffc0] w-40 p-4 mt-3 rounded-md mx-auto"
+            >
+              <Text className="text-center">Accept and continue</Text>
+            </Pressable>
           </View>
-        </Modal>
-        <FormButton
-          text="Delete Account"
-          onPress={() => setModalVisible(!modalVisible)}
-          disabled={loading}
-        />
-      </View>
+        </View>
+      </Modal>
+      <FormButton
+        text="Delete Account"
+        onPress={() => setModalVisible(!modalVisible)}
+        disabled={loading}
+      />
     </View>
   );
 };
